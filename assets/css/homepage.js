56b7b466810b9c1ee4606ee2218c8a99eb602e5e
Doug Red <douglasred63@yahoo.com>
2022-07-02 13:51:17 -0500

    added if errors

--- assets/css/homepage.js
+++ assets/css/homepage.js
@@ -8,11 +8,21 @@ var getUserRepos = function (user) {
   var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
   // make a request to the url
-  fetch(apiUrl).then(function (response) {
-    response.json().then(function (data) {
-      displayRepos(data, user);
+  fetch(apiUrl)
+    .then(function (response) {
+      // request was succesful
+      if (response.ok) {
+        response.json().then(function (data) {
+          displayRepos(data, user);
+        });
+      } else {
+        alert("Error: GitHub user was Not Found");
+      }
+    })
+    .catch(function (error) {
+      // Notice this `.catch() getting chainedonto the end of the `.then()` method
+      alert("Unable to connect to GitHub");
     });
-  });
 };
 
 var formSubmitHandler = function (event) {
@@ -32,6 +42,12 @@ var displayRepos = function (repos, searchTerm) {
   // clear old content
   repoContainerEl.textContent = "";
   repoSearchTerm.textContent = searchTerm;
+
+  // check if api returned any repo
+  if (repos.length === 0) {
+    repoContainerEl.ContainerEl.textContent = "No repositories found.";
+    return;
+  }
 
   // loop over repos
   for (var i = 0; i < repos.length; i++) {
