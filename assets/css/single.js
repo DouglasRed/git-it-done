7ae46e426042e0a5d8e566c8568c8da94edf4c4a
Doug Red <douglasred63@yahoo.com>
2022-07-02 15:40:28 -0500

    addede more detail for UI

--- assets/css/single.js
+++ assets/css/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function (repo) {
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -9,6 +10,10 @@ var getRepoIssues = function (repo) {
       response.json().then(function (data) {
         // Pass response data to dom function
         displayIssues(data);
+        // check if api has paginated issues
+        if (response.headers.get("Link")) {
+          displayWarning(repo);
+        }
       });
     } else {
       alert("There was a problem with your request!");
@@ -26,7 +31,7 @@ var displayIssues = function (issues) {
     // Create a link element to take to the issues on GitHub
     var issueEl = document.createElement("a");
     issueEl.classList = "list-item flex-row justify-space-between align-center";
-    issueEl.setAttribute("href", "issues[i].html_url");
+    issueEl.setAttribute("href", issues[i].html_url);
     issueEl.setAttribute("target", "_blank");
 
     //create span to hold issue title
@@ -51,4 +56,16 @@ var displayIssues = function (issues) {
   }
 };
 
-getRepoIssues("DouglasRed/coding-quiz-challenge");
+var displayWarning = function (repo) {
+  // add text to warning container
+  limitWarningEl.textContent = "To see more than 30 issues, visit ";
+  var linkEl = document.createElement("a");
+  linkEl.textContent = "See More Issues on Github.com";
+  linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+  linkEl.setAttribute("target", "_blank");
+
+  // append to warning container
+  limitWarningEl.appendChild(linkEl);
+};
+
+getRepoIssues("facebook/react");
